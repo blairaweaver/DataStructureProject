09015d33be9eaaebbc91d013dc393e41cc1522a2
blairweaver <bw09135@georgiasouthern.edu>
2018-11-08 16:31:59 -0500

    Started working on the actual tree. Most of the generic methods are there. Need to finish the process for adding a new node and start working on the process for removing a node.

--- RedBlackTree.java
+++ RedBlackTree.java
@@ -24,6 +24,7 @@ public class RedBlackTree<K extends Comparable<K>, E> {
             return key;
         }
 
+//        Black is 0, red is 1
         public int getAux() {
             return aux;
         }
@@ -69,5 +70,94 @@ public class RedBlackTree<K extends Comparable<K>, E> {
             if (parent.rightChild == this) return parent.leftChild;
             else return parent.rightChild;
         }
+    }
+
+    private RBNode root;
+    private int blackHeight = 0;
+
+    public RedBlackTree() {
+        root = null;
+    }
+
+    public RedBlackTree(K k, E e) {
+        root = new RBNode(k, e, null, 0);
+        root.setLeftChild(new RBNode(null, null, root, 0));
+        root.setRightChild(new RBNode(null, null, root, 0));
+//        This doesn't count the dummy nodes. Maybe change this later?
+        blackHeight = 1;
+    }
+
+    public RBNode getRoot() {
+        return root;
+    }
+
+    public int getBlackHeight() {
+        return blackHeight;
+    }
+    
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+//    External node is considered a dummy node
+    public boolean isExternal(RBNode<K, E> e){
+        return e.getElement() == null;
+    }
+
+//    Internal node is any node that is not a dummy node
+    public boolean isInternal(RBNode<K, E> e) {
+        return e.getElement() != null;
+    }
+
+    public boolean isRoot(RBNode<K, E> e) {
+        return root == e;
+    }
+
+    public void inOrder(RBNode<K, E> e) {
+        if (e.getLeftChild() != null) inOrder(e.getLeftChild());
+        if (e.getElement() != null) System.out.print(e.getElement());
+        if (e.getRightChild() != null) inOrder(e.getRightChild());
+    }
+
+    public void preOrder(RBNode<K, E> e) {
+        if (e.getElement() != null) System.out.print(e.getElement());
+        if (e.getLeftChild() != null) preOrder(e.getLeftChild());
+        if (e.getRightChild() != null) preOrder(e.getRightChild());
+    }
+
+    public void postOrder(RBNode<K, E> e) {
+        if (e.getLeftChild() != null) postOrder(e.getLeftChild());
+        if (e.getRightChild() != null) postOrder(e.getRightChild());
+        if (e.getElement() != null) System.out.print(e.getElement());
+    }
+
+    public RBNode<K, E> treeSearch(K k, RBNode<K, E> e) {
+        if (isExternal(e)) return e;
+        int cmp = k.compareTo(e.getKey());
+        if (cmp < 0) return treeSearch(k, e.getLeftChild());
+        else if (cmp == 0) return e;
+        else return treeSearch(k, e.getRightChild());
+    }
+
+    public void insert(K k, E e) {
+        RBNode<K, E> search = treeSearch(k, root);
+        if (isExternal(search)) {
+            add(search, k, e);
+//            Need to do checks here to make sure tree satisfies the rules
+        }
+        else updateNode(search, e);
+    }
+
+    private void add(RBNode<K, E> old, K k, E e) {
+        old.setKey(k);
+        old.setElement(e);
+        old.setAux(1);
+        old.setLeftChild(new RBNode<>(null, null, old, 0));
+        old.setRightChild(new RBNode<>(null, null, old, 0));
+    }
+
+//    Just updating a node if the key already exist, won't change the Red/Black nature of the tree
+    private void updateNode(RBNode<K, E> old, E e) {
+        old.setElement(e);
     }
 }
